aa442fd5fbdde83332ba72a03b7470143277196d
visualstudio <visualstudio@230-PC03>
2019-02-04 17:01:15 -0500

    change by putting functions

--- c0731867-week5/js/main.js
+++ c0731867-week5/js/main.js
@@ -1,4 +1,5 @@
-﻿
+﻿function peanut() {
     var date = new Date();
-        document.body.innerHTML = "<h1> the date is "+ date +" </h1>"
-    
+    document.body.innerHTML = "<h1> the date is " + date + " </h1>"
+
+}
